6c6d7187a00d709a92581a6619949152d6c81919
Dhruvrajx27 <chudasamadhruvraj8@gmail.com>
2024-11-23 14:46:16 +0530

    Add files via upload

---
+++ PROGRAM1.c
@@ -0,0 +1,14 @@
+// Documentation  : Print Your Name.
+// header file.
+
+#include<stdio.h>
+
+int main(){
+
+    // print Your Name
+    printf("dhruvraj");
+     printf("\n\n24CE019_DHRUVRAJ");
+
+    return 0;
+}
+
