516bcd02c3da96d0c1b0a8f8429864573639915f
Dhruvrajx27 <chudasamadhruvraj8@gmail.com>
2024-11-23 14:52:53 +0530

    Add files via upload

---
+++ PROGRAM3.c
@@ -0,0 +1,22 @@
+//Documentation : WAP to Find A Simple Intrest
+//header file
+#include<stdio.h>
+
+int main(){
+
+    // Variable Declaration (Time in Years)
+    float Princeple_Amount = 50000 , rate = 5.5 , time_ = 3 , Simple_Intrest ;
+
+    // Calculate A Simple Intrest.
+      Simple_Intrest = (Princeple_Amount * rate *time_ )/100;
+
+     // Output As Simple Intrest.
+        printf("Your Simple Intrest is :  %f",Simple_Intrest);
+
+          printf("\n\n24CE019_DHRUVRAJ ");
+
+
+
+    return 0;
+}
+
