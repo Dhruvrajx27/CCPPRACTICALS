20b8e1cc2aa37cb9443d97a2c7833e862d9b2c0c
Dhruvrajx27 <chudasamadhruvraj8@gmail.com>
2024-11-23 14:50:47 +0530

    Add files via upload

---
+++ PROGRAM2.c
@@ -0,0 +1,22 @@
+// Documentation: Find A Rectangle Area And Perimeter.
+// header file.
+
+#include<stdio.h>
+
+int main(){
+
+    // Variable Declaration.
+    int length = 50 , Width = 30 , Area , Perimeter ;
+
+    // Calculate a Area Of Rectangle.
+       Area = length*Width;
+    printf("Your Area is :%d\n",Area);
+
+    // Calculate a Perimeter of Rectangle.
+    Perimeter  = 2*(length + Width);
+    printf("Your Perimeter is : %d\n",Perimeter);
+
+    printf("\n\n24CE019_DHRUVRAJ");
+    return 0;
+}
+
