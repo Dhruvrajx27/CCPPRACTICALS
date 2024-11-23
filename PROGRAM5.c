65133c593d5c139e1aac844c9964e44a933ad1c8
Dhruvrajx27 <chudasamadhruvraj8@gmail.com>
2024-11-23 14:56:07 +0530

    Add files via upload

---
+++ PROGRAM5.c
@@ -0,0 +1,22 @@
+// Documentation : Convert The temperature Celsius to Fahrenheit
+// Header File.
+#include<stdio.h>
+
+int main(){
+
+    float  f , c; // c is Celsius.
+
+    // Take a Input As Celsius.
+    printf(" Enter a Temperature :");
+    scanf("%f", &c);
+
+    //Process Of Convert Celsius To Fahrenheit
+     f = ((9*c)/5)+32;
+
+    //Output As Fahrenheit Temperature.
+    printf("\n Temperature In Fahrenheit: %f F\n\n" ,f);   // f is Fahrenheit
+    printf(" 24CE019_DHRUVRAJ");
+
+    return 0;
+}
+
